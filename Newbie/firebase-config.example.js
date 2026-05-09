c8b92007064b69ce839765be15999ceabdeeb6c4
RHWB94 <rhwb94@gmail.com>
2026-05-09 19:57:53 +0800

    security update

---
+++ Newbie/firebase-config.example.js
@@ -0,0 +1,11 @@
+// Copy this file to firebase-config.js and fill it with the restricted,
+// current Firebase Web App config. Do not commit firebase-config.js.
+window.RHWB_FIREBASE_CONFIG = {
+    apiKey: "YOUR_RESTRICTED_FIREBASE_WEB_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    databaseURL: "https://YOUR_PROJECT-default-rtdb.REGION.firebasedatabase.app",
+    projectId: "YOUR_PROJECT",
+    storageBucket: "YOUR_PROJECT.firebasestorage.app",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_WEB_APP_ID"
+};
