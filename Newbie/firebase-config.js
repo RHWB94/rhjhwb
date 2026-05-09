6775d80e86aa8ee43fe67cfbcd04c19174e6c755
RHWB94 <rhwb94@gmail.com>
2026-05-09 19:59:48 +0800

    update

---
+++ Newbie/firebase-config.js
@@ -0,0 +1,11 @@
+// Copy this file to firebase-config.js and fill it with the restricted,
+// current Firebase Web App config. Do not commit firebase-config.js.
+window.RHWB_FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCJArVheLQ8lb2Sineqo7WQ3xvAUjzFs6o",
+    authDomain: "rhwbnewbie.firebaseapp.com",
+    databaseURL: "https://rhwbnewbie-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "rhwbnewbie",
+    storageBucket: "rhwbnewbie.firebasestorage.app",
+    messagingSenderId: "65957218510",
+    appId: "1:65957218510:web:9a5dec94bc81d578ca689f"
+};
